db1c1d91e37d4a4938efaa180daa499dc7371d4a
Roxas2402 <98048769+Roxas2402@users.noreply.github.com>
2022-11-16 13:21:46 +0100

    <

--- src/HiloHijo.java
+++ src/HiloHijo.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class HiloHijo {
+import java.util.concurrent.Semaphore;
+
+public class HiloHijo extends Thread {
+
+    //TODO 4: Creamos el contador y el contador total
+    public static int cont;
+    public static int contTotal = 0;
+    private Semaphore semaphore;
+
+    //TODO 2: Creamos el constructor
+    public HiloHijo(String name, Semaphore semaphore) {
+        super(name);
+        this.semaphore = semaphore;
+    }
+    @Override
+    public void run() {
+        //TODO 6: Añadimos los colores del Main al semáforo e incrementamos los contadores
+        try {
+            semaphore.acquire();
+            cont = 0;
+            for (String color : Main.colores) {
+                if (color.equalsIgnoreCase(this.getName())) {
+                    cont++;
+                    contTotal++;
+                }
+            }
+            System.out.println("Soy " + this.getName() + ", tengo " + cont + " colores.");
+            semaphore.release();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.concurrent.Semaphore;
+
 public class Main {
+
+    //TODO 3: Creamos el Array
+    public static String[] colores;
+
+    //TODO 5: Creamos el static con los colores que tendrá el semáforo
+    static {
+        colores = new String[]{"rojo", "verde", "azul", "rojo", "verde"};
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //TODO 1: Creamos el hijo
+        //TODO 7: Añadimos los colores al semáforo y los runeamos
+        Semaphore semaphore = new Semaphore(1);
+        HiloHijo rojo = new HiloHijo("rojo", semaphore);
+        HiloHijo verde = new HiloHijo("verde", semaphore);
+        HiloHijo azul = new HiloHijo("azul", semaphore);
+
+        rojo.start();
+        verde.start();
+        azul.start();
+
+        try {
+            rojo.join();
+            verde.join();
+            azul.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Total colores: " + HiloHijo.contTotal);
+
     }
 }
